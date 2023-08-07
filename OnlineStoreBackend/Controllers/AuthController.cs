b912b4904cbb58264dbc4f601c3a66d835e83ab1
Windler00 <Windler00@gmail.com>
2023-08-08 02:03:31 +0600

    added login method and validation methods

--- OnlineStoreBackend/Controllers/AuthController.cs
+++ OnlineStoreBackend/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OnlineStoreBackend.Dto;
 using OnlineStoreBackend.Models;
+using OnlineStoreBackend.Services;
 
 namespace OnlineStoreBackend.Controllers
 {
@@ -13,6 +14,14 @@ namespace OnlineStoreBackend.Controllers
         public async Task<ActionResult> Registration(RegistrationDto request)
         {
             User user = new User();
+            if(!AuthService.IsValidEmail(request.Email))
+            {
+                return BadRequest("Email incorrect");
+            }
+            if (!AuthService.IsValidPassword(request.Password))
+            {
+                return BadRequest("Password incorrect(Minimum 1 uppercase, letter 1 symbol, minimum length 8 symbols)");
+            }
             if(request.Email != request.EmailConfirmation)
             {
                 return BadRequest("Email and email confirmation is not same");
@@ -25,9 +34,39 @@ namespace OnlineStoreBackend.Controllers
             {
                 user.Email = request.Email;
                 user.PasswordHash = request.Password;
+                DataContext.Users.Add(user);
                 return Ok(user);
             }
+        }
 
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginDto request)
+        {
+            var user = DataContext.Users.Find(u => u.Email.Contains(request.Email));
+            if (!AuthService.IsValidEmail(request.Email))
+            {
+                return BadRequest("Email is incorrect");
+            }
+            if (!AuthService.IsValidPassword(request.Password))
+            {
+                return BadRequest("Password incorrect(Minimum 1 uppercase, letter 1 symbol, minimum length 8 symbols)");
+            }
+            if (request.Password != request.PasswordConfirmation)
+            {
+                return BadRequest("Passwords not same");
+            }
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+            if (user.PasswordHash != request.Password)
+            {
+                return BadRequest("Wrong password");
+            }
+            else
+            {
+                return Ok(user);
+            }
         }
     }
 }
---
+++ OnlineStoreBackend/Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnlineStoreBackend.Services
+{
+    public class AuthService
+    {
+        public static bool IsValidPassword(string password)
+        {
+            return Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$");
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            Regex regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            return regex.IsMatch(email);
+        }
+    }
+}
