d03920afc1a65b36b08717ad3ffacde8d81a87bc
Windler00 <Windler00@gmail.com>
2023-10-25 15:47:34 +0600

    added price to product model

--- OnlineStoreBackend/Models/Product.cs
+++ OnlineStoreBackend/Models/Product.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public List<User> Owners { get; set; }
+
         public string ImageUrl { get; set; } = string.Empty;
         public List<Category> Categories { get; set; }
     }
